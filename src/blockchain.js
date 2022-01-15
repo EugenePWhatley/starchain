1f6b6a4e5a0d52c32b6a0234829e4ce1064e1371
Eugene Whatley <epwhatley@gmail.com>
2022-01-15 11:31:32 -0500

    Add block to the chain

--- src/blockchain.js
+++ src/blockchain.js
@@ -64,7 +64,24 @@ class Blockchain {
     _addBlock(block) {
         let self = this;
         return new Promise(async (resolve, reject) => {
-           
+            const result = self.getChainHeight().then(height => {
+                console.log("In here")
+                block.previousHash = self.chain[height]?.hash
+                console.log(`New block previous hash: ${block.previousHash}`)
+                block.time = new Date().getTime().toString()
+                console.log(`New block time: ${block.time}`)
+                block.height = self.height + 1
+                console.log(`New block height: ${block.height}`)
+                block.hash = SHA256(JSON.stringify(block))
+                console.log(`New block hash: ${block.hash}`)
+                self.chain.push(block)
+                self.height++
+                return block
+            }).catch(error => {
+                console.log(`Unable to add block: ${error}`)
+                reject(`Unable to add block: ${error}`)
+            })
+            resolve(result)
         });
     }
 
